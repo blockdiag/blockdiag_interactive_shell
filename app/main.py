4b25188da19f668c562bda3b936fa348a7a989af
tk0miya <i.tkomiya@gmail.com>
2011-03-17 23:05:44 +0900

    * Reimplement using Flask

--- app/main.py
+++ app/main.py
@@ -16,153 +16,156 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from utils import setup_noderenderers
 from utils import base64_decode
 
-from blockdiag.elements import *
-import blockdiag
-import seqdiag
+from google.appengine.ext.webapp.util import run_wsgi_app
+import werkzeug
+from flask import Flask, request, render_template
 
 
-class MainPage(webapp.RequestHandler):
-    def get(self):
-        dirname = os.path.dirname(__file__)
-        fpath = os.path.join(dirname, 'templates', 'index.html')
-        params = {}
-
-        source = self.request.get('src')
-        if source:
-            params['diagram'] = base64_decode(source)
-
-        html = template.render(fpath, params)
-
-        self.response.headers['Content-Type'] = 'application/xhtml+xml'
-        self.response.out.write(html)
+app = Flask(__name__)
+app.debug = True
 
 
-class ImagePage(webapp.RequestHandler):
-    def get(self):
-        callback = self.request.get('callback')
-        source = self.request.get('src')
-        encoding = self.request.get('encoding', 'jsonp')
+@app.route('/')
+def blockdiag_index():
+    kwargs = {}
 
+    source = request.args.get('src')
+    if source:
+        kwargs['diagram'] = base64_decode(source)
+
+    body = render_template('index.html', **kwargs)
+    response = app.make_response(body)
+    response.headers['Content-Type'] = 'application/xhtml+xml'
+    return response
+
+
+@app.route('/image', methods=['GET', 'POST'])
+@app.route('/upload/image', methods=['GET', 'POST'])
+def blockdiag_image():
+    if request.method == 'POST':
+        source = request.form['src']
+    else:
+        source = request.args.get('src')
+    encoding = request.args.get('encoding')
+
+    if encoding == 'base64':
+        source = base64_decode(source)
+
+    svg = blockdiag_generate_image(source)
+    if encoding == 'jsonp':
+        callback = request.args.get('callback')
+        if callback and svg:
+            json = simplejson.dumps({'image': svg}, ensure_ascii=False)
+            jsonp = u'%s(%s)' % (callback, json)
+        else:
+            jsonp = ''
+
+        response = app.make_response(jsonp)
+        response.headers['Content-Type'] = 'text/javascript'
+    else:
+        response = app.make_response(svg)
         if encoding == 'base64':
-            source = base64_decode(source)
+            response.headers['Content-Type'] = 'image/svg+xml'
+        else:
+            response.headers['Content-Type'] = 'text/plain'
 
-        svg = self.generate_image(source)
-        if encoding == 'jsonp':
-            if callback and svg:
-                json = simplejson.dumps({'image': svg}, ensure_ascii=False)
-                jsonp = u'%s(%s)' % (callback, json)
-            else:
-                jsonp = ''
-
-            self.response.headers['Content-Type'] = 'text/javascript'
-            self.response.out.write(jsonp)
-        elif encoding == 'base64':
-            self.response.headers['Content-Type'] = 'image/svg+xml'
-            self.response.out.write(svg)
-
-    def post(self):
-        source = self.request.get('src')
-        svg = self.generate_image(source)
-
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write(svg)
-
-    def generate_image(self, source):
-        try:
-            DiagramNode.clear()
-            DiagramEdge.clear()
-
-            tree = blockdiag.diagparser.parse(blockdiag.diagparser.tokenize(source))
-            diagram = blockdiag.blockdiag.ScreenNodeBuilder.build(tree)
-            draw = blockdiag.DiagramDraw.DiagramDraw('SVG', diagram)
-            draw.draw()
-            svg = draw.save('')
-        except Exception, e:
-            self.errors = e
-            svg = ''
-
-        return svg.decode('utf-8')
+    return response
 
 
-class SeqdiagMainPage(webapp.RequestHandler):
-    def get(self):
-        dirname = os.path.dirname(__file__)
-        fpath = os.path.join(dirname, 'templates', 'seqdiag.html')
-        params = {}
+def blockdiag_generate_image(source):
+    import blockdiag
+    from blockdiag.elements import DiagramNode, DiagramEdge
 
-        source = self.request.get('src')
-        if source:
-            params['diagram'] = base64_decode(source)
+    try:
+        DiagramNode.clear()
+        DiagramEdge.clear()
 
-        html = template.render(fpath, params)
+        tree = blockdiag.diagparser.parse(blockdiag.diagparser.tokenize(source))
+        diagram = blockdiag.blockdiag.ScreenNodeBuilder.build(tree)
+        draw = blockdiag.DiagramDraw.DiagramDraw('SVG', diagram)
+        draw.draw()
+        svg = draw.save('')
+    except Exception, e:
+        svg = ''
 
-        self.response.headers['Content-Type'] = 'application/xhtml+xml'
-        self.response.out.write(html)
+    return svg.decode('utf-8')
 
 
-class SeqdiagImagePage(webapp.RequestHandler):
-    def get(self):
-        callback = self.request.get('callback')
-        source = self.request.get('src')
-        encoding = self.request.get('encoding', 'jsonp')
+@app.route('/seqdiag/')
+def seqdiag_index():
+    kwargs = {}
 
+    source = request.args.get('src')
+    if source:
+        kwargs['diagram'] = base64_decode(source)
+
+    body = render_template('seqdiag.html', **kwargs)
+    response = app.make_response(body)
+    response.headers['Content-Type'] = 'application/xhtml+xml'
+    return response
+
+
+@app.route('/seqdiag/image', methods=['GET', 'POST'])
+def seqdiag_image():
+    if request.method == 'POST':
+        source = request.form['src']
+    else:
+        source = request.args.get('src')
+    encoding = request.args.get('encoding')
+
+    if encoding == 'base64':
+        source = base64_decode(source)
+
+    svg = seqdiag_generate_image(source)
+    if encoding == 'jsonp':
+        callback = request.args.get('callback')
+        if callback and svg:
+            json = simplejson.dumps({'image': svg}, ensure_ascii=False)
+            jsonp = u'%s(%s)' % (callback, json)
+        else:
+            jsonp = ''
+
+        response = app.make_response(jsonp)
+        response.headers['Content-Type'] = 'text/javascript'
+    else:
+        response = app.make_response(svg)
         if encoding == 'base64':
-            source = base64_decode(source)
+            response.headers['Content-Type'] = 'image/svg+xml'
+        else:
+            response.headers['Content-Type'] = 'text/plain'
 
-        svg = self.generate_image(source)
-        if encoding == 'jsonp':
-            if callback and svg:
-                json = simplejson.dumps({'image': svg}, ensure_ascii=False)
-                jsonp = u'%s(%s)' % (callback, json)
-            else:
-                jsonp = ''
-
-            self.response.headers['Content-Type'] = 'text/javascript'
-            self.response.out.write(jsonp)
-        elif encoding == 'base64':
-            self.response.headers['Content-Type'] = 'image/svg+xml'
-            self.response.out.write(svg)
-
-    def post(self):
-        source = self.request.get('src')
-        svg = self.generate_image(source)
-
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write(svg)
-
-    def generate_image(self, source):
-        try:
-            DiagramNode.clear()
-            DiagramEdge.clear()
-
-            tree = seqdiag.diagparser.parse(seqdiag.diagparser.tokenize(source))
-            diagram = seqdiag.seqdiag.DiagramTreeBuilder().build(tree)
-            draw = seqdiag.seqdiag.DiagramDraw('SVG', diagram)
-            draw.draw()
-            svg = draw.save('')
-        except Exception, e:
-            self.errors = e
-            svg = ''
-
-        return svg.decode('utf-8')
+    return response
 
 
-class ImageUploadMainPage(webapp.RequestHandler):
-    def get(self):
-        dirname = os.path.dirname(__file__)
-        fpath = os.path.join(dirname, 'templates', 'upload.html')
-        html = template.render(fpath, {})
+def seqdiag_generate_image(source):
+    import seqdiag
+    from blockdiag.elements import DiagramNode, DiagramEdge
 
-        self.response.headers['Content-Type'] = 'application/xhtml+xml'
-        self.response.out.write(html)
+    try:
+        DiagramNode.clear()
+        DiagramEdge.clear()
 
-    def post(self):
+        tree = seqdiag.diagparser.parse(seqdiag.diagparser.tokenize(source))
+        diagram = seqdiag.seqdiag.DiagramTreeBuilder().build(tree)
+        draw = seqdiag.seqdiag.DiagramDraw('SVG', diagram)
+        draw.draw()
+        svg = draw.save('')
+    except Exception, e:
+        self.errors = e
+        svg = ''
+
+    return svg.decode('utf-8')
+
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def blockdiag_upload_form():
+    if request.method == 'GET':
+        return render_template('upload.html')
+    else:
         import png
-        from StringIO import StringIO
         from itertools import izip
 
-        io = StringIO(self.request.get('img'))
-        pict = png.Reader(file=io)
+        pict = png.Reader(file=request.files['img'])
         x, y, pixels, meta = pict.asRGB8()
 
         diagram = """diagram{
@@ -193,26 +196,12 @@ class ImageUploadMainPage(webapp.RequestHandler):
         diagram += "}\n"
 
 
-        dirname = os.path.dirname(__file__)
-        fpath = os.path.join(dirname, 'templates', 'upload2.html')
-        html = template.render(fpath, {'diagram': diagram})
-
-        self.response.headers['Content-Type'] = 'application/xhtml+xml'
-        self.response.out.write(html)
+        body = render_template('upload2.html', diagram=diagram)
+        response = app.make_response(body)
+        response.headers['Content-Type'] = 'application/xhtml+xml'
+        return response
 
 
-application = webapp.WSGIApplication([('/', MainPage),
-                                      ('/image', ImagePage),
-                                      ('/seqdiag/', SeqdiagMainPage),
-                                      ('/seqdiag/image', SeqdiagImagePage),
-                                      ('/upload', ImageUploadMainPage)],
-                                     debug=True)
-
-
-def main():
+if __name__ == '__main__':
     setup_noderenderers()
-    run_wsgi_app(application)
-
-
-if __name__ == "__main__":
-    main()
+    run_wsgi_app(app)
