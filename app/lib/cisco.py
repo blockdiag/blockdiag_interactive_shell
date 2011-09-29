c342457a97676b5fa41e660e8ebc65f5dc010c31
tk0miya <i.tkomiya@gmail.com>
2011-09-30 00:47:42 +0900

    * Update to blockdiagcontrib-cisco-0.1.3

--- app/lib/cisco.py
+++ app/lib/cisco.py
@@ -18,6 +18,7 @@ import os
 from blockdiag.noderenderer import NodeShape
 from blockdiag.noderenderer import install_renderer
 from blockdiag.utils.XY import XY
+from blockdiag.utils import images
 
 try:
     from blockdiag.utils.PILTextFolder import PILTextFolder as TextFolder
@@ -38,7 +39,10 @@ def gen_image_class(image_path, baseurl=None):
             self.textalign = 'left'
             self.image_path = image_path
 
-            size = self.image_size
+            box = metrix.cell(node).box()
+            bounded = (box[2] - box[0], box[3] - box[1])
+            size = images.get_image_size(image_path)
+            size = images.calc_image_size(size, bounded)
 
             pt = metrix.cell(node).center()
             self.image_box = [pt.x - size[0] / 2, pt.y - size[1] / 2,
@@ -59,102 +63,11 @@ def gen_image_class(image_path, baseurl=None):
             self.connectors[2] = XY(pt.x, self.image_box[3])
             self.connectors[3] = XY(self.image_box[0], pt.y)
 
-        @property
-        def image_size(self):
-            try:
-                import PIL.Image
-                raise ImportError()
-
-                size = PIL.Image.open(image_path).size
-            except ImportError:
-                size = JpegFile.get_size(image_path)
-
-            box = [self.metrix.nodeWidth, self.metrix.nodeHeight]
-
-            if box[0] < size[0] or box[1] < size[1]:
-                if (size[0] * 1.0 / box[0]) < (size[1] * 1.0 / box[1]):
-                    size = (size[0] * box[1] / size[1], box[1])
-                else:
-                    size = (box[0], size[1] * box[0] / size[0])
-
-            return size
-
         def render_shape(self, drawer, format, **kwargs):
             if not kwargs.get('shadow'):
                 drawer.loadImage(image_url, self.image_box)
 
     return CiscoImage
-
-
-class StreamReader(object):
-    def __init__(self, stream):
-        self.stream = stream
-        self.pos = 0
-
-    def read_byte(self):
-        byte = self.stream[self.pos]
-        self.pos += 1
-        return ord(byte)
-
-    def read_word(self):
-        byte1, byte2 = self.stream[self.pos:self.pos + 2]
-        self.pos += 2
-        return (ord(byte1) << 8) + ord(byte2)
-
-    def read_bytes(self, n):
-        bytes = self.stream[self.pos:self.pos + n]
-        self.pos += n
-        return bytes
-
-
-class JpegHeaderReader(StreamReader):
-    M_SOI = 0xd8
-    M_SOS = 0xda
-
-    def read_marker(self):
-        if self.read_byte() != 255:
-            raise ValueError("error reading marker")
-        return self.read_byte()
-
-    def skip_marker(self):
-        """Skip over an unknown or uninteresting variable-length marker"""
-        length = self.read_word()
-        self.read_bytes(length - 2)
-
-    def __iter__(self):
-        while True:
-            if self.read_byte() != 255:
-                raise ValueError("error reading marker")
-
-            marker = self.read_byte()
-            if marker == self.M_SOI:
-                length = 0
-                data = ''
-            else:
-                length = self.read_word()
-                data = self.read_bytes(length - 2)
-
-            yield (marker, data)
-
-            if marker == self.M_SOS:
-                raise StopIteration()
-
-
-class JpegFile(object):
-    M_SOF0 = 0xc0
-    M_SOF1 = 0xc1
-
-    @classmethod
-    def get_size(self, filename):
-        image = open(filename, 'rb').read()
-        headers = JpegHeaderReader(image)
-        for header in headers:
-            if header[0] in (self.M_SOF0, self.M_SOF1):
-                data = header[1]
-
-                height = (ord(data[1]) << 8) + ord(data[2])
-                width = (ord(data[3]) << 8) + ord(data[4])
-                return (width, height)
 
 
 def to_classname(filename):
