8d9d7331ab451632c87e46008cd877f53590062b
Yoshinori K. Okuji <okuji@enbug.org>
2011-11-04 23:16:53 +0900

    add more

--- app/apps/nwdiag_main.py
+++ app/apps/nwdiag_main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from lib.utils import base64_decode, get_redirect_url, simplejson
+from lib.utils import decode_source, get_redirect_url, simplejson
 from flask import Blueprint, redirect, request, make_response, render_template
 
 app = Blueprint('nwdiag_main', __name__)
@@ -16,7 +16,8 @@ def nwdiag_index():
 
     source = request.args.get('src')
     if source:
-        kwargs['diagram'] = base64_decode(source)
+        compression = request.args.get('compression')
+        kwargs['diagram'] = decode_source(source, 'base64', compression)
 
     body = render_template('nwdiag.html', **kwargs)
     response = make_response(body)
@@ -30,10 +31,11 @@ def nwdiag_image():
         source = request.form['src']
     else:
         source = request.args.get('src')
-    encoding = request.args.get('encoding')
 
-    if encoding == 'base64':
-        source = base64_decode(source)
+    encoding = request.args.get('encoding')
+    compression = request.args.get('compression')
+
+    source = decode_source(source, encoding, compression)
 
     format = request.args.get('format', 'SVG')
     image = nwdiag_generate_image(source, format)
