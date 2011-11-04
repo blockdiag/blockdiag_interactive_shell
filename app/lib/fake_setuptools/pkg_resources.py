77983c37d54037b6ed8353f857b1e540ad3ee77f
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-11-04 19:27:34 +0900

    * Support blockdiag 1.0.0 (plugin feature)

--- app/lib/fake_setuptools/pkg_resources.py
+++ app/lib/fake_setuptools/pkg_resources.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 
+from blockdiag.utils.collections import namedtuple
+
+plugins = {}
+
+
+def fake_entry_point(module):
+    EntryPoint = namedtuple('EntryPoint', 'load')
+    return EntryPoint(lambda: module)
+
+
 def declare_namespace(*args, **kwargs):
     pass
 
+
 def iter_entry_points(*args, **kwargs):
-    return []
+    name = args[1]
+    if name in plugins:
+        ep = fake_entry_point(plugins[name])
+        return [ep]
+    else:
+        return []
